077d54cd350ff29f44eb7fe07bc4dd87ae91a5cf
Matestampa <paredpiso5@gmail.com>
2024-01-28 20:03:01 -0300

    Save and Cancel turno service ref

--- src/services/turnos_Service.js
+++ src/services/turnos_Service.js
@@ -35,7 +35,7 @@ async function get_availHours(day){
 }
 
 //POST "turnos/save"
-async function save_turno(day_timeId){
+async function save_turno(day_timeId,user_id){
     
     //Traer day del day_time
     let response=await pool.query("SELECT day FROM day_time WHERE id=$1",[day_timeId]);
@@ -65,7 +65,9 @@ async function save_turno(day_timeId){
     
     //Si no ,agregamos un nuevo turno y disminuimos la cant de espacios para ese day_time
     else{
-       let newTurno_id;
+       
+       let newTurno_id=(await conn.query(`INSERT INTO turnos(user_id,day_time_id) 
+                                        VALUES($1,$2) RETURNING id`,[user_id,day_timeId])).rows[0].id;
        
        //disminuimos espacios
        await conn.query(`UPDATE day_time SET avail=avail-1 WHERE id=$1`,[day_timeId])
@@ -94,10 +96,11 @@ async function cancel_turno(turno_id,user_id){
     //Traer dia y dayTime_id de la db usando tmb el user id, para ver q sea de el user q lo pide
     let response=await pool.query(`SELECT day_time.id,day_time.day FROM turnos INNER JOIN day_time 
                                  ON turnos.day_time_id=day_time.id WHERE turnos.id=$1 
-                                 AND turnos.user_id=$2`,[turno_id, user_id]);
+                                 AND turnos.user_id=$2`,[turno_id,user_id]);
     
     //Si no Trae nada, bad request
-    if (response.rows[0]==undefined){return { error:DFLT_API_ERRORS.BAD_REQ() }}
+    console.log(response.rows);
+    if (response.rows[0]==undefined){return { error:DFLT_API_ERRORS.BAD_REQ("Private turno") }}
     
     //Si es coorecto tomamos la data
     let turno_day=response.rows[0].day;
@@ -106,7 +109,6 @@ async function cancel_turno(turno_id,user_id){
     
     //Chequear que el dia este al tiempo correcto para cancelar.
     if (day_diference(new Date(turno_day),new Date()) < const_vars.MINIMUM_DAYS_2CANCEL){
-        
         return {error:DFLT_API_ERRORS.BAD_REQ("Days limit to cancel reached") };
     }
     
@@ -133,7 +135,7 @@ async function cancel_turno(turno_id,user_id){
         //Hacemos "commit" de la transaction
         try{
           transacction.commit();
-          return;
+          return {};
         }
         catch(e){
             conn.release();
